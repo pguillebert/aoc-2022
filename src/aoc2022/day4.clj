52759ebc02cea2c6427e715238063778bffa946e
Philippe Guillebert <philippe@bench.co>
2022-12-04 11:06:18 -0800

    AOC 2022 day 4 cleanup

--- src/aoc2022/day4.clj
+++ src/aoc2022/day4.clj
@@ -11,25 +11,21 @@
         expa (range (Integer/parseInt a1) (inc (Integer/parseInt a2)))
         [b1 b2] (str/split b #"-")
         expb (range (Integer/parseInt b1) (inc (Integer/parseInt b2)))]
-    [expa expb]))
+    [(into #{} expa) (into #{} expb)]))
 
 (defn find-contained
-  [[r1 r2]]
-  (let [r1set (into #{} r1)
-        r2set (into #{} r2)]
-    (or
-     (set/subset? r1set r2set)
-     (set/subset? r2set r1set))))
+  [[r1set r2set]]
+  (or
+   (set/subset? r1set r2set)
+   (set/subset? r2set r1set)))
 
 (defn find-overlap
-  [[r1 r2]]
-  (let [r1set (into #{} r1)
-        r2set (into #{} r2)
-        inter (set/intersection r1set r2set)]
-    (not (empty? inter))))
+  [[r1set r2set]]
+  (not (empty? (set/intersection r1set r2set))))
 
-(defn day4-1
-  []
+;; either call with find-contained or find-overlap
+(defn day4
+  [func]
   (let [lines (-> (io/resource file)
                   (slurp)
                   (str/split #"\n"))
@@ -37,19 +33,6 @@
     (->> pairs
 ;;         (take 4)
          (map expand-ranges)
-         (map find-contained)
-         (filter identity)
-         (count))))
-
-(defn day4-2
-  []
-  (let [lines (-> (io/resource file)
-                  (slurp)
-                  (str/split #"\n"))
-        pairs (map #(str/split % #",") lines)]
-    (->> pairs
-;;         (take 20)
-         (map expand-ranges)
-         (map find-overlap)
+         (map func)
          (filter identity)
          (count))))
