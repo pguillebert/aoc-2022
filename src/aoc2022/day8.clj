8b782f504a56ef7160003b3641c142a94ed18c26
Philippe Guillebert <philippe@bench.co>
2022-12-09 19:21:15 -0800

    AOC 2022 day 8 part 2

--- src/aoc2022/day8.clj
+++ src/aoc2022/day8.clj
@@ -6,34 +6,77 @@
 
 (def file "input8.txt")
 
-(defn scan-mark [dir list]
+(defn scan-mark [list]
   (second
    (reduce (fn [[max-seen acc] tree]
              (if (> tree max-seen)
-               [tree (conj acc {:item tree :visible true :direction dir})]
-               [max-seen (conj acc {:item tree :visible false})]
+               [tree (conj acc {:height tree :visible true})]
+               [max-seen (conj acc {:height tree :visible false})]
                ))
            [-1 []]
            list)))
 
-(defn merge-one-tree [& tree-maps]
-  (assert (apply = (map :item tree-maps)) "different trees heights?!")
+(defn visibility-merge
+  [v1 v2]
+    (if (and (boolean? v1) (boolean? v2))
+      (or v1 v2)
+      ;; non boolean: it's the :height key, same value expected. pick v2
+      v2))
+
+(defn merge-one-tree [mergefunc & tree-maps]
+  (assert (apply = (map :height tree-maps)) "different trees heights?!")
   (apply merge-with
-         (fn [v1 v2]
-           (if (and (boolean? v1) (boolean? v2))
-             (or v1 v2)
-             ; non boolean: either :item (same value expected)
-             ; or :direction (not useful for final processing). pick v2
-             v2))
+         mergefunc
          tree-maps))
 
-(defn merge-forests [& forests]
+(defn merge-forests [mergefunc & forests]
   (apply map
    (fn [& lines]
-     (apply map merge-one-tree lines))
+     (apply map (partial merge-one-tree mergefunc) lines))
    forests))
 
 (defn transpose [m] (apply mapv vector m))
+
+(defn process-visible
+  [merged-data]
+  (->> merged-data
+       (map (fn [line] (map :visible line)))
+       (flatten)
+       (filter identity)
+       (count)))
+
+(defn mk-score [idx list]
+  (let [our-tree-height (nth list idx)
+        to-the-left (take idx list)
+        smaller-trees (->> to-the-left
+                           (reverse)
+                           (reduce (fn [acc height]
+                                     (if (< height our-tree-height)
+                                       (conj acc height)
+                                       ; if stopped by a tree add one item
+                                       (reduced (conj acc :extra))))
+                                   []))]
+    (count smaller-trees)))
+
+(defn scenic-score [list]
+  (second
+   (reduce
+    (fn [[idx acc] tree]
+      [(inc idx) (conj acc {:height tree
+                            :score (mk-score idx list)})])
+    [0 []]
+    list)))
+
+(defn scenic-merge [v1 v2]
+  ;; note that this will also square the :height value, YOLO
+  (* v1 v2))
+
+(defn process-scenic
+  [merged-data]
+  (->> merged-data
+       (map (fn [line] (map :score line)))
+       (flatten)
+       (apply max)))
 
 (defn day8-data
   []
@@ -45,28 +88,28 @@
                          (vec
                           (map #(Integer/parseInt (str %))
                                (seq s)))))
-                  (vec))
+                  (vec))]
+    data))
 
-        minidata (->> data
-                      (take 6)
-                      (map #(take 6 %)))
+(defn day8 [mark-function merge-function process-function]
+  (let [data (day8-data)
+;        [[3 0 3 7 3]
+;         [2 5 5 1 2]
+;         [6 5 3 3 2]
+;         [3 3 5 4 9]
+;         [3 5 3 9 0]]
 
-        left-fun (comp identity (partial scan-mark :left) identity)
-        right-fun (comp reverse (partial scan-mark :right) reverse)
-        top-fun (comp identity (partial scan-mark :top) identity)
-        bottom-fun (comp reverse (partial scan-mark :bottom) reverse)
+        left-fun mark-function
+        right-fun (comp reverse mark-function reverse)
+        top-fun mark-function
+        bottom-fun (comp reverse mark-function reverse)
 
         left-matches (map left-fun data)
         right-matches (map right-fun data)
         top-matches (transpose (map top-fun (transpose data)))
         bottom-matches (transpose (map bottom-fun (transpose data)))
 
-        merged-data (merge-forests left-matches right-matches top-matches bottom-matches)
-
-        visible (map (fn [line] (map :visible line)) merged-data)]
-
-    (->> visible
-         (flatten)
-         (filter identity)
-         (count))
-    ))
+        merged-data (merge-forests merge-function
+                                   left-matches right-matches top-matches bottom-matches)
+        ]
+    (process-function merged-data)))
